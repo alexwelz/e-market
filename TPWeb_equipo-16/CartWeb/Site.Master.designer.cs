20f36c4cb3ba9b30ea1162895abf8e6183598b96
Alebardock <Alejandrod.morales@hotmail.com>
2023-10-16 14:15:07 -0300

    Accionar busqueda con tecla Enter

--- TPWeb_equipo-16/CartWeb/Site.Master.designer.cs
+++ TPWeb_equipo-16/CartWeb/Site.Master.designer.cs
@@ -33,6 +33,15 @@ namespace CartWeb
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Control btnBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBuscar;
+
+        /// <summary>
         /// Control tbFilter.
         /// </summary>
         /// <remarks>
